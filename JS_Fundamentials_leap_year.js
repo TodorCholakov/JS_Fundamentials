8470e6608273c25f008b471038220c5af2bc182b
Edmant <t.s.cholakov@gmail.com>
2021-01-13 11:09:50 +0200

    Create JS_Fundamentials_leap_year.js

---
+++ JS_Fundamentials_leap_year.js
@@ -0,0 +1,13 @@
+//Finished 100/100
+
+function solve (param1) {
+    let param = Number(param1)
+    if (param%4==0 && param%100!=0 || param%400==0) {
+       console.log ("yes")
+       }  else {
+        console.log ("no")
+       }
+    }
+       
+
+solve (2003)
