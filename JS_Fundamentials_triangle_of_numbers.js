c2514fcf26d5b215911943b3663f3db8fb86cd81
Edmant <t.s.cholakov@gmail.com>
2021-01-13 23:14:36 +0200

    Create JS_Fundamentials_triangle_of_numbers.js

---
+++ JS_Fundamentials_triangle_of_numbers.js
@@ -0,0 +1,20 @@
+//Finished 100/100
+
+function solve (param1) {
+    let num1, res="";
+    let temp = 0;
+   num1 = Number(param1)
+    for (let i=1; i<=num1; i++){
+        
+       for (let y=1; y<=i; y++){
+        resnum = i.toString()
+        res+=`${resnum} `
+        
+        
+       }
+       console.log(res)
+       res="";
+    }
+    
+}
+solve (5)
