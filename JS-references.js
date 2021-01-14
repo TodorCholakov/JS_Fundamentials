d7dc83bf4f9668b2f3fc4611b70e65be5cad4c9f
Edmant <t.s.cholakov@gmail.com>
2021-01-14 16:05:00 +0200

    Update JS-references.js

--- JS-references.js
+++ JS-references.js
@@ -5,3 +5,34 @@ Hello();
 Hello();
 
 //cmd in explorer write - "code ." - отваря папката в VS code
+//повдигане на степен - Math.pow (number, 4)
+function multiply (){
+    let a = Math.pow(2, 4);
+    let b = (2 **4);
+    console.log (a)
+    console.log (b)
+}
+multiply()
+
+//SWITCH
+function switch1 (param){
+ switch (param) {
+     case 1: console.log ("Monday");
+     break;
+     case 2: console.log ("Tuesday");
+     break;
+     default: console.log ("facebook")
+ }
+}
+switch1 (2)
+
+//тернален оператор 
+function dayOfTheWeek (param1){
+    if (param1>5){
+        console.log ("weekend");
+    } else {
+        console.log("wokday");
+    }
+    console.log (param1>5 ? "Weekend" : "Worcday");
+} 
+dayOfTheWeek(4)
