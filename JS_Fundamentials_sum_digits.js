24fcc29cb7ead108ba1b7c84e257efd4b44b3b1b
Edmant <t.s.cholakov@gmail.com>
2021-01-18 23:02:29 +0200

    Create JS_Fundamentials_sum_digits.js

---
+++ JS_Fundamentials_sum_digits.js
@@ -0,0 +1,10 @@
+//OK 100/100
+function solve (param){
+    let temp = 0
+    let stringify = param.toString().split("");
+    let toNumber = stringify.map(Number)
+    let res = toNumber.reduce((x, temp)=>x+temp)
+    console.log(res)
+
+}
+solve (12322)
