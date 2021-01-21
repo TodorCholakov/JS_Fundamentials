1d3d6f7836a7ee4a3df39618084ff7f503fcded7
Edmant <t.s.cholakov@gmail.com>
2021-01-21 16:23:24 +0200

    Create JS_Fundamentials_Arrays_Add_And_Substract.js

---
+++ .vscode/JS_Fundamentials_Arrays_Add_And_Substract.js
@@ -0,0 +1,27 @@
+//100/100
+
+function solve (param1) {
+    let sum1 = 0;
+    let sum2 = 0;
+    for(let a of param1){
+        sum1+=a
+    }
+    let res=[];
+    let temp;
+   for (let i = 0; i<param1.length; i++){
+       if (param1[i]%2==0){
+           temp =  param1[i]+=i
+            res.push(temp)
+       } else {
+        temp = param1[i]-=i
+        res.push(temp)
+       }
+   }
+   console.log (res)
+   for(let a of param1){
+    sum2+=a
+}
+   console.log (sum1)
+   console.log (sum2)
+}
+solve ([5, 15, 23, 56, 35])
