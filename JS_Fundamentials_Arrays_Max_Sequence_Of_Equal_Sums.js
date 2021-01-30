5be5fdcebc853549bfa76657133a4a9fa3a5367a
Edmant <t.s.cholakov@gmail.com>
2021-01-30 10:52:50 +0200

    Update JS_Fundamentials_Arrays_Max_Sequence_Of_Equal_Sums.js

--- JS_Fundamentials_Arrays_Max_Sequence_Of_Equal_Sums.js
+++ JS_Fundamentials_Arrays_Max_Sequence_Of_Equal_Sums.js
@@ -2,21 +2,27 @@
 
 function solve (arr){
     let count=0;
-    for (let i=0; i<arr.length; i++){
-        if (arr[i]==arr[i+1]){
-            count+=1;
-            
-        } else {
-            
-        }
-        console.log (count)
+    res=[]
+    let compCount=0;
+    let temIndexMin=0;
+    let temIndexMax=0;
+
+   for (let i=0; i<arr.length; i++){
+       
+    if (arr[i] == arr[i+1]){
+        count+=1;
+        temIndexMin = i;
+    } else if(compCount<count){
+        compCount=count;
+        count=0;
+    } 
+    else {
+        temIndexMax = i;
     }
+   }
+   for (let i=temIndexMin-1; i<=temIndexMax-1; i++){
+        res.push(arr[i])
+   }
+   console.log (res.join(" "))
 }
-solve(["1050",
-    "200",
-    "450",
-    "2",
-    "18.50" ,
-    "16.86",
-    "special"
-]);
+solve([0, 1, 1, 5, 2, 2, 6, 3, 3]);
